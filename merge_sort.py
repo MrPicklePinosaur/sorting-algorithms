fbf6bffc7d08056e2a8d6946ee218bbbfa8a5b1b
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-07-29 13:18:51 -0400

    merge sort done

--- merge_sort.py
+++ merge_sort.py
@@ -6,8 +6,8 @@ def merge_sort(arr):
           return arr
 
      mid = int((len(arr)+1)/2)
-     arr1 = merge_sort(arr[0:mid])
-     arr2 = merge_sort(arr[mid:len(arr)])
+     arr1 = merge_sort(arr[:mid])
+     arr2 = merge_sort(arr[mid:])
 
      for i in arr2: #for each value that we want to insert
           sorted_insert(i,arr1)
@@ -16,23 +16,18 @@ def merge_sort(arr):
      return arr1
 
 def sorted_insert(n_val,host_array): #sorted insert assuming host array is sorted least to greatest
-     for ind in range(len(host_array)-1,-1,-1):
+     for ind in range(len(host_array)):
           h_val = host_array[ind] #get the current val in host_array
-          if (n_val >= h_val):
+          if n_val < h_val:
                host_array.insert(ind,n_val)
                return
           
-     host_array.append(n_val) #if array has index of zero
+     host_array.append(n_val) #if array is empty
 
-'''
-arr = [i for i in range(5)]
+
+arr = [i for i in range(200)]
 r.shuffle(arr)
 print(arr)
 merge_sort(arr)
 
-'''
-arr = []
-for i in range(10):
-     sorted_insert(r.randint(0,30),arr)
-     print(arr)
 
